49342dd69fdaf879c959855441fdffe602029799
Camilo Cespedes <Camilo Cespedes@LAPTOP-V65MCEG1>
2020-06-06 10:05:52 -0500

    Interfaz demo. Sin funcionalidad

---
+++ src/Interfaz/Button.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaz;
+
+import java.awt.Image;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Camilo Cespedes
+ */
+public class Button extends JButton{
+    Image bLook;
+    JPanel cont, sig;
+}
