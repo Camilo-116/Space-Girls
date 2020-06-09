dd78923f82bf794f3416344a1d53602ca01b5fa9
Familia Velasquez <Familia Velasquez@192.168.39.168>
2020-06-09 06:50:43 -0500

    En el nivel de Katherine  no se pudo aplicar ek movimiento.

---
+++ src/Estado/Estado.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Estado;
+
+/**
+ *
+ * @author Camilo Cespedes
+ */
+public enum Estado {
+    INICIO, AYUDA, CONFIG, SELECCION,
+HISK, HISM, HISD, IN_DIALOG, NOT_IN_DIALOG;
+}
---
+++ src/Resources/Seccion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Resources;
+
+import java.awt.Image;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Camilo Cespedes
+ */
+public class Seccion {
+    ArrayList<Image> imagenes;
+}
